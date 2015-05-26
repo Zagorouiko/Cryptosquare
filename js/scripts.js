6f39e3b474f9f1fd92f03327a5663f9f2506c116
Leon Zagorouiko & ian bevel <student@epicodus.com>
2015-05-26 16:16:48 -0700

    update scripts to have a normalizeMessage and sqrtMessage function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
-var encrypt = function(message) {
-  message = message.replace(/\s+/g, "");
+var normalizeMessage = function(message) {
+  var message = message.replace(/\s+/g, "");
   message = message.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-  message = message.toLowerCase()
+  message = message.toLowerCase();
   return message;
+}
+
+var sqrtMessage = function(message) {
+  var normalizedMsg = normalizeMessage(message);
+  var messageLength = normalizedMsg.length
+  var sqrtMessage = Math.sqrt(messageLength);
+  sqrtMessage = Math.ceil(sqrtMessage)
+  return sqrtMessage;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,14 @@
 describe("encrypt", function() {
   it("removes all the spaces from the sentence", function() {
-    expect(encrypt("hello world")).to.equal("helloworld");
+    expect(normalizeMessage("hello world")).to.equal("helloworld");
   });
   it("removes all punctuation from the sentence", function() {
-    expect(encrypt("hello world,.!")).to.equal("helloworld");
+    expect(normalizeMessage("hello world,.!")).to.equal("helloworld");
   });
   it("lower cases all uppcased letters from the sentence.", function() {
-    expect(encrypt("HELLO WORLD")).to.equal("helloworld");
+    expect(normalizeMessage("HELLO WORLD")).to.equal("helloworld");
+  });
+  it("gets the square root of the sentence", function() {
+    expect(sqrtMessage("iamu")).to.equal(2);
   });
 });
